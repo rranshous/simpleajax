f07517ed25531f36fbbf9dc7867e808541b99fd1
Robby Ranshous <rranshous@gmail.com>
2014-03-10 20:37:39 -0400

    simple code for adding items to a list, as recorded in class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simpleajax::Application.config.secret_key_base = 'dd56fba7df9c559a1249f97406967d877ba1d647e188a151eda6d9a66ca1a73ac6ddebed55a318bc048b91ea41feb98122ee7582da037f803d75dc111cb4433c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Simpleajax::Application.config.session_store :cookie_store, key: '_simpleajax_session'
